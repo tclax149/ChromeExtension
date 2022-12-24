38be08a45c1dec79f8ec2e3e0a6fc9ded6c9e4b6
Tywan Claxton <tywan149@yahoo.com>
2022-12-24 07:32:50 -0700

    LeadsSaver1.0

--- extension.js
+++ extension.js
@@ -5,31 +5,49 @@ const inputButton = document.getElementById("input-button");
 const inputElement = document.getElementById("input-element");
 const unorderedListEl = document.getElementById("unorderedList");
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const deleteButton = document.getElementById("delete-button")
+const saveTabButton = document.getElementById("save-tab")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
+
+
+
+function renderLeads(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+     listItems += `
+        <li>
+             <a target='_blank' href='${leads[i]}'>
+             ${leads[i]}  
+             </a>
+        </li>
+        `
+    }
+ unorderedListEl.innerHTML = listItems
+}
+
+saveTabButton.addEventListener("click", function(){
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads)
+    })
+
+})
+
+deleteButton.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    renderLeads(myLeads);
+})
 
 inputButton.addEventListener("click", function() {
     myLeads.push(inputElement.value)
     inputElement.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+    renderLeads(myLeads)
 })
-
-function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
- listItems += `
-    <li>
-         <a target='_blank' href='${myLeads[i]}'>
-         ${myLeads[i]}  
-         </a>
-    </li>
-    `
-}
-
-    unorderedListEl.innerHTML = listItems;
-
-}
