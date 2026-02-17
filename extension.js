81bbd64c0863efb1ae4724f129e61db7a979be86
Tywan Claxton <tywan149@yahoo.com>
2026-02-17 11:13:58 -0500

    last update

--- extension.js
+++ extension.js
@@ -30,7 +30,6 @@ function renderLeads(leads) {
 }
 
 saveTabButton.addEventListener("click", function(){
-    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     myLeads.push(tabs[0].url)
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
